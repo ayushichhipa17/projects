7b3ce91f082c3219a22c90cda18edf21e76265f3
Ayush <ayushchhipa7@gmail.com>
2024-06-02 16:42:38 +0530

    the new file added on heeloji

---
+++ heeloji.java
@@ -0,0 +1,19 @@
+
+// package com.company;
+
+import java.util.Scanner;
+
+public class heeloji {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter one number");
+        int first = sc.nextInt();
+        System.out.println("Enter second number");
+        int second = sc.nextInt();
+
+        int sum = first + second;
+        System.out.println("the two number of sum is " + sum);
+
+    }
+}
