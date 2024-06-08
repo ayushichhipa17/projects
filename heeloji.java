b77d71d2ab7d372124c3d2db3b7d0e2e5794a735
ayushichhipa17 <ayushichhipa17@gmail.com>
2024-06-08 18:08:09 +0530

    pettern program java

--- heeloji.java
+++ heeloji.java
@@ -1,3 +1,74 @@
-public class heeloji {
-    // hello java project for new braches like allprojects check
-}
+// Pyramid of Stars
+
+// public class heeloji {
+// public static void printStarPattern(int n) {
+// for (int i = 1; i <= n; i++) {
+// for (int j = 1; j <= i; j++) {
+// System.out.print("*");
+// }
+// System.out.println();
+// }
+// }
+
+// public static void main(String[] args) {
+// int n = 5;
+// printStarPattern(n);
+// }
+// }
+
+// Pyramid of Numbers
+
+// public class heeloji {
+// public static void printNumberPattern(int n) {
+// for (int i = 1; i <= n; i++) {
+// for (int j = 1; j <= i; j++) {
+// System.out.print(j);
+// }
+// System.out.println();
+// }
+// }
+
+// public static void main(String[] args) {
+// int n = 5;
+// printNumberPattern(n);
+// }
+// }
+
+// Repeating Numbers
+
+// public class heeloji {
+// public static void printSameNumberPattern(int n) {
+// for (int i = 1; i <= n; i++) {
+// for (int j = 1; j <= i; j++) {
+// System.out.print(i);
+// }
+// System.out.println();
+// }
+// }
+
+// public static void main(String[] args) {
+// int n = 5;
+// printSameNumberPattern(n);
+// }
+// }
+
+// Alphabet Pattern
+
+// public class heeloji {
+// public static void printCharrPattern(int n) {
+// char ch = 'A';
+// for (int i = 1; i <= n; i++) {
+// for (int j = 1; j <= i; j++) {
+// System.out.print(ch);
+
+// }
+// ch++;
+// System.out.println();
+// }
+// }
+
+// public static void main(String[] args) {
+// int n = 5;
+// printCharrPattern(n);
+// }
+// }
