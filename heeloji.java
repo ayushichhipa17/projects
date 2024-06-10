083adae763c76fa287de15590559c870942629fd
ayushichhipa17 <ayushichhipa17@gmail.com>
2024-06-10 23:58:53 +0530

    new change in java

--- heeloji.java
+++ heeloji.java
@@ -1,74 +1,16 @@
-// Pyramid of Stars
+import java.util.Scanner;
 
-// public class heeloji {
-// public static void printStarPattern(int n) {
-// for (int i = 1; i <= n; i++) {
-// for (int j = 1; j <= i; j++) {
-// System.out.print("*");
-// }
-// System.out.println();
-// }
-// }
+public class heeloji {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-// public static void main(String[] args) {
-// int n = 5;
-// printStarPattern(n);
-// }
-// }
+        System.out.println("Enter one number:");
+        int first = sc.nextInt();
 
-// Pyramid of Numbers
+        System.out.println("Enter second number:");
+        int second = sc.nextInt();
 
-// public class heeloji {
-// public static void printNumberPattern(int n) {
-// for (int i = 1; i <= n; i++) {
-// for (int j = 1; j <= i; j++) {
-// System.out.print(j);
-// }
-// System.out.println();
-// }
-// }
-
-// public static void main(String[] args) {
-// int n = 5;
-// printNumberPattern(n);
-// }
-// }
-
-// Repeating Numbers
-
-// public class heeloji {
-// public static void printSameNumberPattern(int n) {
-// for (int i = 1; i <= n; i++) {
-// for (int j = 1; j <= i; j++) {
-// System.out.print(i);
-// }
-// System.out.println();
-// }
-// }
-
-// public static void main(String[] args) {
-// int n = 5;
-// printSameNumberPattern(n);
-// }
-// }
-
-// Alphabet Pattern
-
-// public class heeloji {
-// public static void printCharrPattern(int n) {
-// char ch = 'A';
-// for (int i = 1; i <= n; i++) {
-// for (int j = 1; j <= i; j++) {
-// System.out.print(ch);
-
-// }
-// ch++;
-// System.out.println();
-// }
-// }
-
-// public static void main(String[] args) {
-// int n = 5;
-// printCharrPattern(n);
-// }
-// }
+        int sum = first + second;
+        System.out.println("Sum: " + sum);
+    }
+}
