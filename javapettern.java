8f0546eb6b3546ac5f7e83287584b51a7091931b
ayushichhipa17 <ayushichhipa17@gmail.com>
2024-06-13 23:48:13 +0530

    petter of java

--- javapettern.java
+++ javapettern.java
@@ -1,6 +1,73 @@
+import java.util.Scanner;
 
 public class javapettern {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // for(int i = 1; i<=5; i++){
+        //     for(int j = 1; j<=5; j++){
+        //         if((i == 1 || j == 1 ) ||(j == 5 || i ==5)){
+        //             System.out.print("*");
+        //         }else{
+        //             System.out.print(" "); 
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // for (int i = 4;i>=1;i--){
+        //     for (int j = 1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+
+        // }
+
+
+        // int num = 1;
+        // for(int i = 1;i<=4;i++){
+        //     for (int j=1;j<=i;j++){
+        //         System.out.print(num);
+        //         num++;
+        //     }
+        //     System.out.println();
+
+        // }
+
+
+        // for(int i =1;i<=4;i++){
+        //     for(int j = 1;j<=i;j++){
+        //         if ((i + j) % 2 == 0) {
+        //             System.out.print("1");
+        //         } else {
+        //             System.out.print("0");
+        //         }
+        //     }
+        //     System.out.println();
+
+
+        // }
+
+
+
+        // for (int i =1;i<=5;i++){
+        //     for (int j = i; j < 5; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=i;j>=1;j--){
+        //         System.out.print(j);
+        //     }
+            
+        //     for (int j = 2; j <= i; j++) {
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+
+        // }
+
+
+
+
     }
 }
